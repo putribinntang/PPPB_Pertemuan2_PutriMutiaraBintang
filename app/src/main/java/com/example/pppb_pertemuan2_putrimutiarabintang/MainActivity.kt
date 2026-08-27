a27c87a536979bbee274f1e226a75874d01ea5f8
praktikan <putribintang.ofc@gmail.com>
2026-08-27 09:38:55 +0700

    update

--- app/src/main/java/com/example/pppb_pertemuan2_putrimutiarabintang/MainActivity.kt
+++ app/src/main/java/com/example/pppb_pertemuan2_putrimutiarabintang/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.pppb_pertemuan2_putrimutiarabintang
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.pppb_pertemuan2_putrimutiarabintang.databinding.ActivityMainBinding
 
-// buat variable untuk binding
-private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // buat variable untuk binding
+    private lateinit var binding: ActivityMainBinding
+
+    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,6 +26,24 @@ class MainActivity : AppCompatActivity() {
 
         // akses ui element di code bloc
         with(receiver = binding) {
+            //akses ui by id
+            tvNumber.text = counter.toString()
+
+            //ketika button count click
+            btnCount.setOnClickListener {
+                counter++
+                tvNumber.text = counter.toString()
+            }
+            // ketika button toast click
+            btnToast.setOnClickListener {
+                Toast
+                    .makeText(
+                        this@MainActivity,
+                        "Counter: $counter",
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+            }
 
         }
     }
