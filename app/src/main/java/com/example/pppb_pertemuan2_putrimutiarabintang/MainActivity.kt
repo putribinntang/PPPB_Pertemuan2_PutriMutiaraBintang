7668c2a51dbd1c39c86c5bf1e262a8539b7d673e
praktikan <putribintang.ofc@gmail.com>
2026-08-27 09:04:24 +0700

    add count app

--- app/src/main/java/com/example/pppb_pertemuan2_putrimutiarabintang/MainActivity.kt
+++ app/src/main/java/com/example/pppb_pertemuan2_putrimutiarabintang/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pppb_pertemuan2_putrimutiarabintang.databinding.ActivityMainBinding
+
+// buat variable untuk binding
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // inisiasi binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // set content dari activity untuk menggunakan binding view root
+        setContentView(binding.root)
+
+        // akses ui element di code bloc
+        with(receiver = binding) {
+
         }
     }
 }
